d1bfcf459f86e6606739e92f68b6cb6ae00a65a7
PhorsBeatRmx <phorsbeatrmx0509@gmail.com>
2024-09-17 05:05:21 -1200

    Connect to Docker

--- src/schema/productCreateSchema.ts
+++ src/schema/productCreateSchema.ts
@@ -4,6 +4,7 @@ const productCreateSchema = Joi.object({
     name: Joi.string().required(),
     price: Joi.number().required(),
     description: Joi.string().required(),
+    date: Joi.date().required(),
 });
 
 export default productCreateSchema;
